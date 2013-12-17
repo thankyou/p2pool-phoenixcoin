2e08bc752dc6d7722a3a655e25385e616bb44b58
spynappels <stefan.pynappels@sptechnical.co.uk>
2013-12-17 14:20:18 +0000

    Update networks.py
    NOT READY YET

--- p2pool/bitcoin/networks.py
+++ p2pool/bitcoin/networks.py
@@ -199,6 +199,27 @@ nets = dict(
         DUMB_SCRYPT_DIFF=2**16,
         DUST_THRESHOLD=0.03e8,
     ),
+    phoenixcoin=math.Object(
+        P2P_PREFIX='fbc0b6db'.decode('hex'),
+        P2P_PORT=9555,
+        ADDRESS_VERSION=14,
+        RPC_PORT=9554,
+        RPC_CHECK=defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
+            'phoenixcoinaddress' in (yield bitcoind.rpc_help()) and
+            not (yield bitcoind.rpc_getinfo())['testnet']
+        )),
+        SUBSIDY_FUNC=lambda height: 200*100000000 >> (height + 1)//3360000,
+        POW_FUNC=lambda data: pack.IntType(256).unpack(__import__('ltc_scrypt').getPoWHash(data)),
+        BLOCK_PERIOD=90, # s
+        SYMBOL='PXC',
+        CONF_FILE_FUNC=lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Phoenixcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Phoenixcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.phoenixcoin'), 'phoenixcoin.conf'),
+        BLOCK_EXPLORER_URL_PREFIX='http://explorer.phoenixcoin.org/block/',
+        ADDRESS_EXPLORER_URL_PREFIX='http://explorer.phoenixcoin.org/address/',
+        TX_EXPLORER_URL_PREFIX='http://explorer.phoenixcoin.org/tx/',
+        SANE_TARGET_RANGE=(2**256//1000000000 - 1, 2**256//1000 - 1),
+        DUMB_SCRYPT_DIFF=2**16,
+        DUST_THRESHOLD=0.03e8,
+    ),
 
 
 )
